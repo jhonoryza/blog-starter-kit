71ec390f2127b204df0d96e40ac716ad451a4645
Ryan Chandler <ryangjchandler@gmail.com>
2021-12-17 22:28:06 +0000

    chore: change PostResource icon

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -19,7 +19,7 @@ class PostResource extends Resource
 {
     protected static ?string $model = Post::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-newspaper';
 
     public static function form(Form $form): Form
     {
