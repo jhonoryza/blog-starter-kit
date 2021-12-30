a4ab0257cc0024e50ea1933f955e0fa7b92047ea
Ryan Chandler <ryangjchandler@gmail.com>
2021-12-30 16:03:11 +0000

    feature: footer & copyright

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -10,7 +10,7 @@
     <script src="{{ mix('js/app.js') }}" defer></script>
     <title>@isset($title) {{ $title }} &mdash; @endisset{{ config('app.name') }}</title>
 </head>
-<body class="antialiased bg-white font-sans pb-12">
+<body class="antialiased bg-white font-sans">
     <nav class="flex items-center justify-between max-w-3xl py-4 mx-auto">
         <span class="inline-flex items-center justify-center w-10 h-10 bg-gray-100 rounded-lg">
             🚀
@@ -36,5 +36,13 @@
     <main class="max-w-3xl mx-auto mt-24">
         {{ $slot }}
     </main>
+
+    <footer class="border-t border-gray-300 mt-12 py-2">
+        <div class="max-w-3xl mx-auto">
+            <span class="font-medium text-xs text-gray-500">
+                &copy; {{ date('Y') }} Your Name. All rights reserved.
+            </span>
+        </div>
+    </footer>
 </body>
 </html>
